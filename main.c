5d6c5512cc624a360b4449cb650e39a411a28627
pedroGabrielDeAndrade <112401590+pedroGabrielDeAndrade@users.noreply.github.com>
2022-10-10 23:28:54 -0300

    Finalizando Programa
    Alterações nas faixas 3 e 4 do programa <3.

--- main.c
+++ main.c
@@ -1,27 +1,27 @@
 /*
 ==================================================================================
-                    Calculadora de Cï¿½digos de Cores para Resistores
+                    Calculadora de Códigos de Cores para Resistores
 ==================================================================================
 Empresa: Espartana
 Equipe de Programadores:
-               Fabrï¿½cio Razera;
+               Fabrício Razera;
                Gabriel de Oliveira Buchara;
                Gabriel Toshio;
-               Letï¿½cia Alves Korzeniewski;
+               Letícia Alves Korzeniewski;
                Pedro Gabriel de Andrade;
                Rafael Vieira Rodrigues.
-Curso: Tecnologia em Anï¿½lise e Desenvolvimento de Sistemas
-Data de inï¿½cio do projeto: 03/10/2022
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas
+Data de início do projeto: 03/10/2022
 ----------------------------------------------------------------------------------
-Descriï¿½ï¿½o do Programa:
-		O programa tï¿½m o objetivo de ajudar na realizaï¿½ï¿½o do cï¿½lculo
-        da resistï¿½ncia elï¿½trica, por meio do uso de cï¿½digo de cores de 4 faixas.
-        O usuï¿½rio deverï¿½ informar as cores das 4 faixas do resistor. Com isso, o
-        programa irï¿½ informar qual serï¿½ o valor da resistï¿½ncia e da sua tolerï¿½ncia,
+Descrição do Programa:
+		O programa tem o objetivo de ajudar na realização do cálculo
+        da resistência elétrica, por meio do uso de código de cores de 4 faixas.
+        O usuário deverá informar as cores das 4 faixas do resistor. Com isso, o
+        programa irá informar qual será o valor da resistência e da sua tolerância,
         todos com a unidade de medida em Ohms.
-Versï¿½o do programa: 1.0
-Melhorias da versï¿½o: Primeira versï¿½o.
-Data de lanï¿½amento da versï¿½o: 11/10/2022
+Versão do programa: 1.0
+Melhorias da versão: Primeira versão.
+Data de lançamento da versão: 11/10/2022
 ==================================================================================
 */
 
@@ -29,35 +29,37 @@ Data de lanï¿½amento da versï¿½o: 11/10/2022
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h> //biblioteca caracteres especiais
-#include <math.h>
 #include <string.h>
 
 void instrucoes() {
     //Boas-vindas ao programa
     printf("*******************************************************************************************");
-    printf("\nOlï¿½, seja bem vindo(a) ï¿½ calculadora de resistï¿½ncia elï¿½trica para resistores de 4 faixas!");
-    printf("\n\nVocï¿½ deverï¿½ informar as cores de quatro faixas.");
-    printf("\nApï¿½s informar as cores corretamente, o programa calcularÃ¡ a resistÃªncia em ohms.");
+    printf("\nOlá, seja bem vindo(a) à calculadora de resistência elétrica para resistores de 4 faixas!");
+    printf("\n\nVocê deverá informar as cores de quatro faixas.");
+    printf("\nAo informar as cores corretamente, o programa calculará a resistência em ohms.");
 
     printf("\n*******************************************************************************************\n");
     //Mostrar os valores das faixas
-    printf("\n\n---------------- Cores ------------------\n");
-    printf("Preto\t\tMarrom\t\tVermelho\nLaranja\t\tAmarelo\t\tVerde\nAzul\t\tVioleta\t\tCinza\nBranco\t\tDourado\t\tPrateado\n");
+    printf("\n\n------------------ Cores ------------------\n");
+    printf("Preto\t\tMarrom\t\tVermelho\nLaranja\t\tAmarelo\t\tVerde\nAzul\t\tVioleta\t\tCinza\nBranco\t\tDourado\t\tPrata\n");
     printf("\n-----------------------------------------------\n");
 
 }
 
-int pega_digito(char *cor) {
+int pega_digito(char cor[]) {
     /*
-        Esta funÃ§Ã£o recebe o nome de uma cor e converte para um dÃ­gito por comparaÃ§Ã£o com um vetor de strings contendo as cores.
+        Esta função recebe o nome de uma cor e converte para um dígito por comparação com um vetor de strings contendo as cores.
 
-        Retorna: o dÃ­gito correspondente Ã  cor, ou -1 se a cor informada for invÃ¡lida.
+        Retorna: o dígito correspondente à cor, ou -1 se a cor informada for inválida.
     */
-    int digito;
+    int i, digito;
 
-    char cores[][15] = {"preto", "marrom", "vermelho", "laranja", "amarelo", "verde", "azul", "violeta", "cinza", "branco", "dourado", "prateado"};
-    for (int i = 0; i < sizeof(cores) / sizeof(cores[0]); i++) {
-        if (!strcmp(cor, cores[i])) {
+    char cores[][15] = {"preto", "marrom", "vermelho", "laranja", "amarelo", "verde", "azul", "violeta", "cinza", "branco"};
+    
+    char coresFem[][15] = {"preta", "marrom", "vermelha", "laranja", "amarela", "verde", "azul", "violeta", "cinza", "branca"};
+    
+    for ( i = 0; i < sizeof(cores) / sizeof(cores[0]); i++) {
+        if (!strcmp(cor, cores[i]) || !strcmp(cor, coresFem[i])) {
             digito = i;
             break;
         } else {
@@ -69,14 +71,110 @@ int pega_digito(char *cor) {
 }
 
 
-void converteLowerCase(char *str) {
+void converteLowerCase(char str[]) {
     /*
-        Esta funÃ§Ã£o converte a string para lower case, para que o usuÃ¡rio nÃ£o seja obrigado a utilizar um case especÃ­fico.
+        Esta função converte a string para lower case, para que o usuário não seja obrigado a utilizar um case específico.
     */
-    for (int i = 0; i < sizeof(str) / sizeof(str[0]); i++) {
+    int i;
+    
+    for ( i = 0; i < sizeof(str) / sizeof(str[0]); i++) {
        str[i] = tolower(str[i]);
     }
 }
+
+
+float pegaMult(char multiplicador[]){
+	
+	
+		if (!strcmp(multiplicador, "preto") || !strcmp(multiplicador, "preta")){
+	
+			return 1;
+		}
+		else if (!strcmp(multiplicador, "marrom")){
+			
+			return 10;
+		}
+		else if (!strcmp(multiplicador, "vermelho")){
+			
+			return 100;
+		}
+		else if (!strcmp(multiplicador, "laranja")){
+			
+			return 1000;
+		}
+		else if (!strcmp(multiplicador, "amarelo") || !strcmp(multiplicador, "amarela")){
+		
+			return 10000;
+		}
+		else if (!strcmp(multiplicador, "verde")){
+			
+			return 100000;
+		}
+		else if (!strcmp(multiplicador, "dourado") || !strcmp(multiplicador, "dourada")){
+			
+			return 0.1;
+		}
+		else if (!strcmp(multiplicador, "prata")){
+			
+			return 0.01;
+		}
+		else{
+		
+			return -1;
+		}
+	
+}
+
+
+
+float pegaTolerancia(char tolerancia[]){
+
+	float tole;
+		
+	if (!strcmp(tolerancia, "preto") || !strcmp(tolerancia, "preto")){
+	
+		tole = 0.2;	
+	}
+	
+	else if (!strcmp(tolerancia, "marrom")){
+		
+		tole = 0.01;
+	}
+	
+	else if (!strcmp(tolerancia, "vermelho") || !strcmp(tolerancia, "vermelha")){
+		
+		tole = 0.02;
+	}
+	
+	else if (!strcmp(tolerancia, "laranja")){
+		
+		tole = 0.03;
+	}
+	
+	else if (!strcmp(tolerancia, "amarelo") || !strcmp(tolerancia, "amarela")){
+		
+		tole = 0.04;
+	}
+	
+	else if (!strcmp(tolerancia, "dourado") || !strcmp(tolerancia, "dourada")){
+		
+		tole = 0.05;
+	}
+	
+	else if (!strcmp(tolerancia, "prata")){
+		
+		tole = 0.1;
+	}
+	
+	else {
+	
+		tole = -1;
+	}
+	
+	return tole;
+}
+
+	
 
 
 float calcVal(){
@@ -86,7 +184,7 @@ float calcVal(){
     char nome_cor[15];
 
     //Receber valores das faixas
-    //Condiï¿½ï¿½o para receber o valor da faixa 1
+    //Condição para receber o valor da faixa 1
     do {
         printf("\nDigite a cor da primeira faixa: ");
         scanf(" %s", nome_cor);
@@ -96,67 +194,72 @@ float calcVal(){
 
 
         if (fx1 == -1)
-            printf("Cor informada invÃ¡lida.");
+            printf("Cor informada inválida.");
         else
-            printf("DÃ­gito relativo Ã  cor: %d", fx1);
+            printf("Dígito relativo à cor: %d", fx1);
     } while (fx1 == -1);
 
-    //Condiï¿½ï¿½o para receber o valor da faixa 2
+    //Condição para receber o valor da faixa 2
     do {
-        printf("\nDigite a cor da segunda faixa: ");
+        printf("\n\nDigite a cor da segunda faixa: ");
         scanf(" %s", nome_cor);
 
         converteLowerCase(nome_cor);
         fx2 = pega_digito(nome_cor);
 
         if (fx2 == -1)
-            printf("Cor informada invÃ¡lida.");
+            printf("Cor informada inválida.");
         else
-            printf("DÃ­gito relativo Ã  cor: %d", fx2);
+            printf("Dígito relativo à cor: %d", fx2);
             
     } while (fx2 == -1);
 
-    //Condiï¿½ï¿½o para receber o valor da faixa 3
+    //Condição para receber o valor da faixa 3
     do {
-        printf("Digite o cï¿½digo da terceira faixa de cor (multiplicador): ");
-        scanf("%f", &fx3);
-    } while (fx3 != 0 && fx3 != 1 && fx3 != 2 && fx3 != 3 && fx3 != 4 && fx3 != 5 && fx3 != 10 && fx3 != 11);
+        printf("\n\nDigite a cor da terceira faixa (multiplicador): ");
+        scanf(" %s", nome_cor);
+		
+		converteLowerCase(nome_cor);
+		fx3 = pegaMult(nome_cor);
+		if(fx3 == -1){
+		
+			printf("Cor informada inválida.");
+		}
+		else{	
+			printf("Multiplicador correspondente à faixa é: %.2f", fx3 );
+		}
+		   
+	} while (fx3 == -1);
 
-//Condiï¿½ï¿½o para receber o valor da faixa 4
+	//Condição o para receber o valor da faixa 4
     do {
-        printf("Digite o cï¿½digo da quarta faixa de cor (tolerï¿½ncia): ");
-        scanf("%f", &fx4);
-    } while (fx4 != 0 && fx4 != 1 && fx4 != 2 && fx4 != 3 && fx4 != 4 && fx4 != 10 && fx4 != 11);
+        printf("\n\nDigite o código da quarta faixa de cor (tolerância): ");
+        scanf(" %s", nome_cor);
+        converteLowerCase(nome_cor);
+        fx4 = pegaTolerancia(nome_cor);
+        
+        if (fx4 == -1){
+        	printf("Cor informada inválida.");
+		}
+		else{
+		
+			printf("Tolerância correspondente é: %.2f%%", fx4 * 100);
+		}
+		
+    } while (fx4 == -1);
 
-//Condiï¿½ï¿½es para o fx3 (Dourado e Prata)
-    if (fx3 == 10){
-        fx3 = -1;
-    }
-    else if (fx3 == 11){
-        fx3 = -2 ;
-    }
-//Condiï¿½ï¿½es para o fx4 (Dourado, Prateado e Preto)
-    if (fx4 == 10){
-        fx4 = 5;
-    }
-    else if (fx4 == 11){
-        fx4 = 10;
 
-    }
-    else if (fx4 == 0){
-        fx4 = 20;
-    }
-//Cï¿½lculo da resistï¿½ncia
-    calc = ((fx1 * 10) + fx2) * (pow(10, fx3)); //funï¿½ï¿½o pow() serve como uma potï¿½ncia. Ex: pow(10,fx3) = 10^fx3.
+	//Cálculo da resistência
+    calc = ((fx1 * 10) + fx2) * (fx3);
 
 //Mostrar valores
-    printf("\nO valor da resistï¿½ncia e da sua tolerï¿½ncia, respectivamente: %.2f ohms +/- %.2f%%. \n",calc,fx4);
-//Convertendo o valor para a realizaï¿½ï¿½o do cï¿½lculo
-    tolerancia = (calc * (fx4/100));
+    printf("\n\nO valor da resistência e da sua tolerância, respectivamente: %.2f ohms +/- %.2f%%. \n",calc,fx4 * 100);
+//Convertendo o valor para a realizaçãoo do cálculo
+    tolerancia = (calc * (fx4));
     calcMenor = (calc  - tolerancia);
     calcMaior = (calc + tolerancia);
-//Mostrando a taxa do valor da resistï¿½ncia
-    printf("A sua tolerï¿½ncia serï¿½ entre: %.2f e %.2f ohms. \n",calcMenor,calcMaior);
+//Mostrando a taxa do valor da resistência
+    printf("A sua tolerância sería entre: %.2f e %.2f ohms. \n",calcMenor,calcMaior);
 
     printf("\nObrigado por utilizar nosso software! A Equipe Espartana agradece! \n");
 
@@ -166,13 +269,12 @@ float calcVal(){
 
 int main()
 {
-    char cor[15];
-    int digito;
+    
     //Setando em pt-BR
     setlocale(LC_ALL, "Portuguese");
 
 
-    //Mostra as boas vindas ao programa. Alï¿½m, das cores e seus respectivos valores
+    //Mostra as boas vindas ao programa. Além, das cores e seus respectivos valores
     instrucoes();
 
 
